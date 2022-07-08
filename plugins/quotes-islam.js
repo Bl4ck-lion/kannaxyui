f30a25cf5f24ac6f635fadd445c9a5627087a60f
Bl4ck-lion <67418383+Bl4ck-lion@users.noreply.github.com>
2022-07-08 22:29:17 +0700

    Add files via upload

--- plugins/quotes-islam.js
+++ plugins/quotes-islam.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, text }) => {
   let res = await (await fetch('https://zahirr-web.herokuapp.com/api/randomquote/muslim?apikey=zahirgans'))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if(!json.result.data[0]) throw json
-  let { result } = json.result.data[0]
+  if(!json.result.data) throw json
+  let { result } = json.result.data
 m.reply(`${json.result.data}`)
 }
 handler.help = ['quotes-islam']
