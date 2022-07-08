d2f8fd085338bb046f70e979d4417629abe25076
Bl4ck-lion <67418383+Bl4ck-lion@users.noreply.github.com>
2022-07-08 20:19:28 +0700

    Update internet-randommeme.js

--- plugins/internet-randommeme.js
+++ plugins/internet-randommeme.js
@@ -1,11 +1,13 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://api.lolhuman.xyz/api/meme?apikey=Apikeymu'
-	conn.sendButton(m.chat, 'Random Mim', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+	let url = 'https://candaan-api-h590oa540-ardhptr21.vercel.app/api/image/random'
+	if (!res.ok) throw await res.text()
+        let json = await res.json()
+        conn.sendButton(m.chat, 'mim indo :v', author, json.data.url, [['NEXT', `${command}`]], m)
 }
-handler.command = /^(r-meme)$/i
+handler.command = /^(meme)$/i
 handler.tags = ['internet']
-handler.help = ['random-meme']
+handler.help = ['meme']
 
 export default handler
