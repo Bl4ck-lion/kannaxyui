096d4dfbf79dc8a788123fc1724b51d4a4306a22
Bl4ck-lion <67418383+Bl4ck-lion@users.noreply.github.com>
2022-07-08 22:35:52 +0700

    Update internet-memeindo.js

--- internet-memeindo.js
+++ internet-memeindo.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let res = 'https://zahirr-web.herokuapp.com/api/random/meme?apikey=zahirgans'
+	let res = await fetch(`https://zahirr-web.herokuapp.com/api/random/meme?apikey=zahirgans}`)
 	if (!res.ok) throw await res.text()
-        let json = await res.json()
+	let json = await res.json()
         let meme = json.result.url
         conn.sendButton(m.chat, 'mim indo :v', author, meme, [['NEXT', `.meme`]], m)
 }
